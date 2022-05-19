9dbf15674b641b120a1514447e7e7e1ebf4ea2b5
Honza Slesinger <slesinger@gmail.com>
2022-05-19 11:48:08 +0200

    launch fix

--- launch/bobik_robot.launch.py
+++ launch/bobik_robot.launch.py
@@ -8,18 +8,18 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
- 
+
 def generate_launch_description():
- 
+
   # Set the path to this package.
   pkg_share = FindPackageShare(package='bobik_robot').find('bobik_robot')
- 
+
   # Set the path to the RViz configuration settings
   default_rviz_config_path = os.path.join(pkg_share, 'rviz/rviz_basic_settings.rviz')
- 
+
   # Set the path to the URDF file
   default_urdf_model_path = os.path.join(pkg_share, 'urdf/bobik_robot.urdf.xacro')
- 
+
   map_yaml_file = os.path.join(get_package_share_directory('bobik_robot'), 'maps', 'place.yaml')
 
   ########### YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE ##############  
@@ -31,28 +31,28 @@ def generate_launch_description():
   slam = LaunchConfiguration('slam')
   use_rviz = LaunchConfiguration('use_rviz')
   use_sim_time = LaunchConfiguration('use_sim_time')
- 
+
   # Declare the launch arguments  
   declare_urdf_model_path_cmd = DeclareLaunchArgument(
     name='urdf_model', 
     default_value=default_urdf_model_path, 
     description='Absolute path to robot urdf file')
-     
+
   declare_rviz_config_file_cmd = DeclareLaunchArgument(
     name='rviz_config_file',
     default_value=default_rviz_config_path,
     description='Full path to the RVIZ config file to use')
-     
+
   declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
     name='gui',
     default_value='True',
     description='Flag to enable joint_state_publisher_gui')
-   
+
   declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
     name='use_robot_state_pub',
     default_value='True',
     description='Whether to start the robot state publisher')
- 
+
   declare_slam_cmd = DeclareLaunchArgument(
     name='slam',
     default_value='False',
@@ -62,29 +62,35 @@ def generate_launch_description():
     name='use_rviz',
     default_value='True',
     description='Whether to start RVIZ')
-     
+
   declare_use_sim_time_cmd = DeclareLaunchArgument(
     name='use_sim_time',
     default_value='False',
     description='Use simulation (Gazebo) clock if true')
-    
+
 
   # Specify the actions
- 
+
+  # Bobik bridge to mediate data between robot and main over ZeroMQ
+  start_bobik_bridge_node = Node(
+    package='bobik_bridge',
+    executable='bobik_bridge',
+    name='bobik_bridge')
+
   # Publish the joint state values for the non-fixed joints in the URDF file.
   start_joint_state_publisher_cmd = Node(
     condition=UnlessCondition(gui),
     package='joint_state_publisher',
     executable='joint_state_publisher',
     name='joint_state_publisher')
- 
+
   # A GUI to manipulate the joint state values
   start_joint_state_publisher_gui_node = Node(
     condition=IfCondition(gui),
     package='joint_state_publisher_gui',
     executable='joint_state_publisher_gui',
     name='joint_state_publisher_gui')
- 
+
   # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
   start_robot_state_publisher_cmd = Node(
     condition=IfCondition(use_robot_state_pub),
@@ -133,8 +139,9 @@ def generate_launch_description():
   ld.add_action(declare_use_robot_state_pub_cmd)  
   ld.add_action(declare_use_rviz_cmd) 
   ld.add_action(declare_use_sim_time_cmd)
- 
+
   # Add any actions
+  ld.add_action(start_bobik_bridge_node)
   ld.add_action(start_joint_state_publisher_cmd)
   ld.add_action(start_joint_state_publisher_gui_node)
   ld.add_action(start_robot_state_publisher_cmd)
